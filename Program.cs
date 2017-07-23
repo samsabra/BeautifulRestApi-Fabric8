a04419ff062fe6b84cda771e0b228e3cdc3ac8f0
Sam Sabra <samsabra@gmail.com>
2017-07-23 17:25:59 -0400

    Update server port, config, and launchSettings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
@@ -8,9 +13,16 @@ namespace BeautifulRestApi
     {
         public static void Main(string[] args)
         {
+           var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://0.0.0.0:5000")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
